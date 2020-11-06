a34894a6c8cca4333f4c4c58a9cbfa4813563af8
menimen <menash9288@gmail.com>
2020-11-06 13:19:00 +0200

    fixed changes

--- client.py
+++
@@ -1,7 +0,0 @@
-import socket
-
-s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-s.sendto(b'Linoy Sela 207654344', ('10.0.2.15', 1234))
-data, addr = s.recvfrom(1024)
-print(str(data), addr)
-s.close()
