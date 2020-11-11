e4a300f5f1667fc883a4cac9dfe0c7ee2057b093
menimen <menash9288@gmail.com>
2020-11-11 19:53:50 +0200

    added print in client for debug

--- ass1/client.py
+++ ass1/client.py
@@ -5,8 +5,10 @@ import sys
 def main():
     serverIP = sys.argv[1]
     serverPort = sys.argv[2]
+    print("trying to cennect to server...")
     s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
     while True:
+        print("connection succesful")
         txt = input("Type your web address and get its ip address: ")
         s.sendto(txt.encode(), (serverIP, int(serverPort)))
         data, addr = s.recvfrom(1024)
