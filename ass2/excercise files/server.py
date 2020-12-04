b990c29e0c839c64951710f45d41293619505406
menimen <menash9288@gmail.com>
2020-12-04 22:46:08 +0200

    i managed to read the file content, but still cant esd to browser

--- ass2/excercise files/server.py
+++ ass2/excercise files/server.py
@@ -17,10 +17,10 @@ def check_if_file_exist(conn, path):
         conn.send(b'Connection: close')
         conn.send(b'\r\n\r\n')
         # conn.close()
-    else:#dont know how to read files especially hmt files
-        # f = open(path, 'rb')
-        # file_content = f.read()
-        # conn.send(file_content)
+    else:  # dont know how to read files especially hmt files
+        f = open(path, 'rb')
+        file_content = f.read()
+        conn.send(file_content)
         conn.send(b'\r\n\r\n')
 
 
@@ -36,7 +36,7 @@ def redirection(conn):
 
 def main():
     flag = 0
-    path = "files/a/oh_no.jpg"
+    path = "files/index.html"
     my_list = [1]
     data = ""  # assign empty string, in the future it will carry the message from client
     address_dict = {'BUFFERSIZE': 1,
@@ -49,20 +49,20 @@ def main():
     s.bind(('', int(myport)))
     s.listen(1)
 
-    while True:  # as long as the list is not empty, we wont close the socket
-        conn, addr = s.accept()
-        # while (flag == 0):
-        chr = conn.recv(1024)  # each time we check if the next char from clien is \r\n\r\n
-        check_if_file_exist(conn, path)
-    # data = data + chr  # concat the last char with the rest of the string
-    # if ((len(data) >= 4) and data[-4:-1] == "\r\n\r\n"):
-    # proccess_client_request(conn, my_list, path,address_dict)  # the client finished the message, now we can proccess it,
-    # redirection(conn,my_list,path,address_dict)
-    # if the file not found or something else unusual we close the socket
-    # if(not my_list) :
-    # flag = 1
-    # conn.send(data.upper())
-
+    # while True:  # as long as the list is not empty, we wont close the socket
+    conn, addr = s.accept()
+    # while (flag == 0):
+    chr = conn.recv(1024)  # each time we check if the next char from clien is \r\n\r\n
+    check_if_file_exist(conn, path)
+    conn.close()
+# data = data + chr  # concat the last char with the rest of the string
+# if ((len(data) >= 4) and data[-4:-1] == "\r\n\r\n"):
+# proccess_client_request(conn, my_list, path,address_dict)  # the client finished the message, now we can proccess it,
+# redirection(conn,my_list,path,address_dict)
+# if the file not found or something else unusual we close the socket
+# if(not my_list) :
+# flag = 1
+# conn.send(data.upper())
 
 
 main()
